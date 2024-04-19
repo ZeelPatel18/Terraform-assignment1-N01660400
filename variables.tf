5f1540c1b83c2f933a1ddc1201035f316827f87a
Zeel Patel <68885863+ZeelPatel18@users.noreply.github.com>
2024-04-18 23:37:38 -0400

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "location" {
+    type = string
+}
+variable "resource_group" {
+}
+variable "storage_account" {
+}
+variable "recovery-vault" {
+  
+}
+variable "log-workspace" {
+  
+}
+variable "tags" {
+    
+}
